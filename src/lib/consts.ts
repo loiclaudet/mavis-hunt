2a78546387d52a64c8915300ad959df76a537337
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-02-22 12:05:25 +0100

    refactor: reduce leaderboard battles count

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -4,7 +4,7 @@ export const DEFAULT_LIST_ELEMENT_SIZE = 161;
 export const BREAKPOINTS = {
   mobile: 640,
 };
-export const LEADERBOARD_PLAYER_BATTLES = 30;
+export const LEADERBOARD_PLAYER_BATTLES = 20;
 export const PROFILE_PLAYER_BATTLES = 100;
 export const MAX_DISPLAYED_PLAYER_BATTLES = 20;
 export const X_RATE_LIMIT_PER_SEC = 10;
