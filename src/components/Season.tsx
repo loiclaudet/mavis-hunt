d10426fe42029c2b8b18948238bd305896e813ca
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-03-06 13:05:47 +0100

    feat: display off-season countdown
    countdown is based on a 40 days season

--- src/components/Season.tsx
+++ src/components/Season.tsx
@@ -9,22 +9,28 @@ export async function Season() {
   if (!season) {
     return <div>Error when fetching season data, please refresh the page</div>;
   }
-  const periodDuration = season.endedAt - DateTime.now().toMillis();
-  const isOffSeason = periodDuration < 0 || periodDuration > SEASON_DURATION;
+  const now = DateTime.now().toMillis();
+  const periodDuration = season.endedAt * 1000 - now;
+  const isOffSeason = periodDuration > SEASON_DURATION;
+  //   const isOffSeason = periodDuration < 0 || periodDuration > SEASON_DURATION;
   return (
     <div className="flex flex-row justify-around sm:flex-col">
       <div className="flex flex-col justify-center">
         <h1 className={`text-2xl sm:text-4xl`}>
           {isOffSeason ? "Off-season" : `🔥 ${season.name} 🔥`}
         </h1>
-        {!isOffSeason && (
-          <div className="mb-3 flex items-center self-center sm:mb-0">
-            <MilliAXSButton />
-            {season.endedAt && (
-              <SeasonCountDown endDate={Number(season.endedAt) * 1000} />
-            )}
-          </div>
-        )}
+        <div className="mb-3 flex items-center self-center sm:mb-0">
+          <MilliAXSButton />
+          {season.endedAt && (
+            <SeasonCountDown
+              endDate={Number(
+                isOffSeason
+                  ? now + (periodDuration - SEASON_DURATION)
+                  : season.endedAt * 1000
+              )}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
