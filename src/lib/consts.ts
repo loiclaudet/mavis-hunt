dfe914cfd50962fa18df9dc001323508967402f7
Lodz <8344874+0xLodz@users.noreply.github.com>
2023-03-08 06:10:03 +0100

    fix: update season duration

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -9,4 +9,4 @@ export const PROFILE_PLAYER_BATTLES = 100;
 export const MAX_DISPLAYED_PLAYER_BATTLES = 20;
 export const X_RATE_LIMIT_PER_SEC = 10;
 export const ORIGIN_RATE_LIMIT_PER_SEC = 5;
-export const SEASON_DURATION = 40 * 24 * 60 * 60 * 1000; // 40 days, in ms
+export const SEASON_DURATION = 42 * 24 * 60 * 60 * 1000; // 42 days, in ms
